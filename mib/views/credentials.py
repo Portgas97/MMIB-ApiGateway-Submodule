558ac34b7efed93df9797e0c500fc5e8dc0a781b
fraventu97 <fraventu97@hotmail.it>
2021-11-30 18:11:41 +0100

    Changed the account data editing, with UserManager

--- mib/views/credentials.py
+++ mib/views/credentials.py
@@ -4,6 +4,8 @@ from flask_login import login_required, current_user
 from mib.forms import CredentialsForm
 from mib.database import db, User
 from mib.views.doc import auto
+from mib.rao.user_manager import UserManager
+
 
 credentials = Blueprint('credentials', __name__)
 
@@ -25,34 +27,44 @@ def _credentials():
     form = CredentialsForm()
     kwargs = {}
     if request.method == 'POST':
-        if form.validate_on_submit():
-            old_password = form.data['old_password']
-            if current_user.authenticate(old_password):
-                for key, value in form.data.items():
-                    # create a dictionary of only meaningful new values
-                    if key != 'old_password' and value != '':
-                        kwargs[key] = value
-                    # also make sure the user doesn't try to change its
-                    # mail to an existing one
-                    if key == 'email':
-                        q = db.session.query(User).filter(
-                            User.email == value)
-                        user = q.first()
-                        if user is not None:
-                            form.email.errors.append("Email already in use")
-                            return render_template(
-                                'error_template.html',
-                                form=form
-                            )
-                if kwargs != {}:
-                    current_user.update(**kwargs)
-                    db.session.commit()
-                    return redirect('/user_data')
-                else:
-                    form.email.errors.append("No changes specified "
-                                             "(How are you here?.")
-            else:
-                form.old_password.errors.append("Wrong Password!")
-        return render_template('error_template.html', form=form)
+
+        # check form validity
+        if not form.validate_on_submit():
+            return render_template('error_template.html', form=form)
+
+        # check user authentication
+        old_password = form.data['old_password']
+        if not UserManager.authenticate(current_user.email, old_password):
+            form.old_password.errors.append("Wrong Password!")
+            return render_template('error_template.html', form=form)
+
+        # display = ['email', 'firstname', 'lastname', 'password',
+            # 'old_password']
+
+        email = form.data["email"] if form.data["email"] != '' \
+            else None
+        firstname = form.data["firstname"] if form.data["firstname"] != '' \
+            else None
+        lastname = form.data["lastname"] if form.data["lastname"] != '' \
+            else None
+        password = form.data["password"] if form.data["password"] != '' \
+            else None
+
+        # check if the new mail already exists
+        if email is not None and UserManager.exist_by_mail(email):
+            form.email.errors.append("Email already in use")
+            return render_template('error_template.html', form=form)
+
+        # if no changes are submitted
+        if not (email or firstname or lastname or password):
+            form.email.errors.append("No changes specified")
+            return render_template('error_template.html', form=form)
+
+        # edit account data
+        UserManager.edit_user(current_user.get_id(), email, firstname,
+                              lastname, None, password)
+        return redirect('/user_data')
+
+    # no POST request
     else:
         return render_template('edit_credentials.html', form=form)
