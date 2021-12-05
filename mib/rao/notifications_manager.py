0a0b3750f2f5fef59fb1334ca6b4d22701ca6050
Francesco <gargiulo.fr@gmail.com>
2021-12-05 13:19:03 -0800

    notifications implementation completed

--- mib/rao/notifications_manager.py
+++ mib/rao/notifications_manager.py
@@ -1,4 +1,5 @@
 from flask import json
+from requests.models import Response
 from werkzeug.security import check_password_hash
 from mib import app
 from flask import abort
@@ -15,6 +16,38 @@ class NotificationsManager:
 
 
     @classmethod
+    def create_notification(cls, email, title, desctiption, timestamp, message_id):
+        notification = Notification()
+        notification.user_email = email
+        notification.title = title
+        notification.description = desctiption
+        notification.timestamp = timestamp
+        notification.message_id = message_id
+
+        try:
+            url = "%s/notification" % cls.NOTIFICATIONS_ENDPOINT
+            response = requests.post(url,
+                                     timeout=cls.REQUESTS_TIMEOUT_SECONDS,
+                                     data=json.dumps(notification),
+                                     headers=encoder.headers)
+        except Exception:
+            return abort(500)
+
+        return response.json()
+
+
+
+    @classmethod
+    def delete_notification(cls, message):
+        try:
+            url = "%s/notification/%s" % (cls.NOTIFICATIONS_ENDPOINT, str(message))
+            response = requests.delete(url, timeout=cls.REQUESTS_TIMEOUT_SECONDS)
+        except Exception:
+            return abort(500)
+        return response.status_code
+
+
+    @classmethod
     def get_notifications(cls, user):
         try:
             url = ("%s/notifications/" % cls.NOTIFICATIONS_ENDPOINT) + user
@@ -24,3 +57,25 @@ class NotificationsManager:
         if response.status_code == 200:
             return response.json()
         return None
+
+
+    @classmethod
+    def notifications_count(cls, user):
+        try:
+            url = "%s/notifications/count/%s" % (cls.NOTIFICATIONS_ENDPOINT, user)
+            response = requests.get(url, timeout=cls.REQUESTS_TIMEOUT_SECONDS)
+        except Exception:
+            return abort(500)
+        if response.status_code == 200:
+            return response.json()
+        return None
+
+
+    @classmethod
+    def set_notifications_as_read(cls, user):
+        try:
+            url = "%s/notifications/%s" % (cls.NOTIFICATIONS_ENDPOINT, user)
+            response = requests.put(url, timeout=cls.REQUESTS_TIMEOUT_SECONDS)
+        except Exception:
+            return abort(500)
+        return response.status_code 
--- mib/views/alerts.py
+++ mib/views/alerts.py
@@ -19,24 +19,27 @@ def notifications():
 
     :return: a rendered view
     """
+
+    
     current_user = flask_login.current_user
     current_user_email = current_user.email
 
     query_notifications = NotificationsManager.get_notifications(current_user_email)
+    NotificationsManager.set_notifications_as_read(current_user_email)
 
     return render_template("notifications.html",
                            notifications=query_notifications)
 
 
-# return
+    # NotificationsManager.create_notification("admin@example.com", "title", "desctiption", "2021-11-11 20:10:19", 1)
+ 
+
+
 def get_notifications_count(user_email):
     """
     :param user_email: the mail we want notification count for
     :return: the number of unread notifications for user_email
     """
-    query = db.session.query(Notification).filter_by(
-        user_email=user_email,
-        is_read=False
-    )
-    notifications_count = query.count()
+
+    notifications_count = NotificationsManager.notifications_count(user_email)
     return notifications_count
