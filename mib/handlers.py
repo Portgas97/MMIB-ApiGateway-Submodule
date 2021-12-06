e963ecfb4033953fbb31a7cb20c0871f19d7195f
giuseppe-crea <7926745+Sylt556@users.noreply.github.com>
2021-12-06 18:02:36 +0100

    Fixed an internal server error on login with unknown user

--- mib/handlers.py
+++ mib/handlers.py
@@ -2,8 +2,8 @@ from flask import render_template
 
 
 def page_404(e):
-    return render_template('404.html'), 404
+    return abort(404)
 
 
 def error_500(e):
-    return render_template('500.html'), 500
+    return abort(500)
